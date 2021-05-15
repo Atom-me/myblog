63ed58ccd8717c8e168534ed244c376c6137012b
liuyaming <sarming@126.com>
2021-05-15 16:57:38 +0800

    发布博客

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -38,7 +38,7 @@
         <div class="text"></div>
         <div class="iptBox">
         </div>
-        <button>发表博客</button>
+        <button @click="onSubmit">发表博客</button>
       </div>
 
     </div>
@@ -66,6 +66,24 @@ export default {
     this.getClassify()
   },
   methods: {
+    onSubmit () {
+      axios({
+        method: 'get',
+        params: this.blogObj,
+        url: 'https://ku.qingnian8.com/dataApi/blog/addBlog.php',
+      }).then(res => {
+        console.log(res, '33333')
+        if (res.data.code === '200') {
+          // alert('发布成功')
+          /**
+           * 在VUe实例内部，你可以通过$router访问路由实例，因此你可以调用 this.$router.push导航到不同的URL
+           */
+          this.$router.push({ path: '/show' })
+        } else {
+          alert('发布失败')
+        }
+      })
+    },
     getClassify () {
       axios({
         method: 'GET',
